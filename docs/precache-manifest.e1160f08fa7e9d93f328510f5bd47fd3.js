2ba79d80a90a499b4a4d3caf8bb659ad4a59474c
Magin <albertomaginusuarez@gmail.com>
2020-08-24 11:01:22 -0300

    asdf

---
+++ docs/precache-manifest.e1160f08fa7e9d93f328510f5bd47fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca8ef4c149ccfc304ca2f1665cc30b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d54dc2465481f32889c5",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5969db4911add7dd6670",
+    "url": "/portfolio/static/js/2.14801e6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.14801e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54dc2465481f32889c5",
+    "url": "/portfolio/static/js/main.a19f5168.chunk.js"
+  },
+  {
+    "revision": "46dd758e01fceb27b502",
+    "url": "/portfolio/static/js/runtime-main.76c32efb.js"
+  }
+]);
